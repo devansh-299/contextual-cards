a403cb3b5c2ea7b457bd81505db1b55bdbd45a06
Devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-11-04 12:01:10 +0530

    added remaining card layouts | updated card adapter

--- app/src/main/java/com/devansh/contextualcards/util/TextFormatter.kt
+++ app/src/main/java/com/devansh/contextualcards/util/TextFormatter.kt
@@ -12,26 +12,31 @@ class TextFormatter {
 
     companion object {
         fun applyFormattedText(
-            formattedText: FormattedText,
+            formattedText: FormattedText?,
             textView: TextView, callBackText: String?) {
-            val spannableStringBuilder = SpannableStringBuilder()
-            for (index in formattedText.entityList.indices) {
-                val entity = formattedText.entityList[index]
-                val colorCode = Color.parseColor(formattedText.entityList[index].color)
-                var spannableString = SpannableString("${entity.text} ")
 
-                spannableString.setSpan(
-                    ForegroundColorSpan(colorCode),
-                    0,
-                    entity.text.length,
-                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-                spannableStringBuilder.append(spannableString)
-            }
-            if (spannableStringBuilder.isEmpty()) {
+            if (formattedText == null) {
                 textView.text = callBackText
             } else {
-                textView.text = spannableStringBuilder
+                val spannableStringBuilder = SpannableStringBuilder()
+                for (index in formattedText.entityList.indices) {
+                    val entity = formattedText.entityList[index]
+                    val colorCode = Color.parseColor(formattedText.entityList[index].color)
+                    var spannableString = SpannableString("${entity.text} ")
+
+                    spannableString.setSpan(
+                        ForegroundColorSpan(colorCode),
+                        0,
+                        entity.text.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    spannableStringBuilder.append(spannableString)
+                }
+                if (spannableStringBuilder.isEmpty()) {
+                    textView.text = callBackText
+                } else {
+                    textView.text = spannableStringBuilder
+                }
             }
         }
     }
--- app/src/main/java/com/devansh/contextualcards/view/adapter/CardAdapter.kt
+++ app/src/main/java/com/devansh/contextualcards/view/adapter/CardAdapter.kt
@@ -1,22 +1,24 @@
 package com.devansh.contextualcards.view.adapter
 
-import android.graphics.drawable.Drawable
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
 import com.devansh.contextualcards.R
 import com.devansh.contextualcards.util.TextFormatter
 import com.devansh.contextualcards.model.Card
 import com.devansh.contextualcards.model.CardGroup
+import com.devansh.contextualcards.util.ImageHelper
 import kotlinx.android.synthetic.main.item_big_display_card.view.*
+import kotlinx.android.synthetic.main.item_center_card.view.*
 import kotlinx.android.synthetic.main.item_image_card.view.*
 import kotlinx.android.synthetic.main.item_small_card_arrow.view.*
+import kotlinx.android.synthetic.main.item_small_display_card.view.*
 
 
+@Suppress("DEPRECATION")
 class CardAdapter(private val designType: CardGroup.DesignType)
     : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
 
@@ -28,8 +30,15 @@ class CardAdapter(private val designType: CardGroup.DesignType)
         when (designType) {
             CardGroup.DesignType.SMALL_CARD_WITH_ARROW ->
                 resourceLayout = R.layout.item_small_card_arrow
+
             CardGroup.DesignType.IMAGE_CARD ->
                 resourceLayout = R.layout.item_image_card
+
+            CardGroup.DesignType.CENTER_CARD ->
+                resourceLayout = R.layout.item_center_card
+
+            CardGroup.DesignType.SMALL_DISPLAY_CARD ->
+                resourceLayout = R.layout.item_small_display_card
         }
         return CardViewHolder(
             LayoutInflater.from(parent.context).inflate(resourceLayout, parent, false)
@@ -68,77 +77,61 @@ class CardAdapter(private val designType: CardGroup.DesignType)
     class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindBigDisplayCard(card: Card) {
-            if (card.formattedTitle != null) {
-                TextFormatter.applyFormattedText(
-                    card.formattedTitle, itemView.bdc_tv_title, card.title)
-            } else {
-                itemView.bdc_tv_title.text = card.title
+            TextFormatter.applyFormattedText(
+                card.formattedTitle, itemView.bdc_tv_title, card.title)
+
+            TextFormatter.applyFormattedText(
+                card.formattedDescription, itemView.bdc_tv_description, card.description)
+
+            card.bgImage?.imageUrl?.let {
+                ImageHelper.loadImage(it, itemView, itemView.bdc_card_view)
             }
-
-            if (card.formattedDescription != null) {
-                TextFormatter.applyFormattedText(
-                    card.formattedDescription, itemView.bdc_tv_description, card.description)
-            } else {
-                itemView.bdc_tv_description.text = card.description
-            }
-
-            Glide.with(itemView)
-                .load(card.bgImage?.imageUrl)
-                .into(object :
-                    CustomTarget<Drawable>() {
-                    override fun onLoadCleared(placeholder: Drawable?) {}
-
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        transition: Transition<in Drawable>?) {
-                        itemView.bdc_card_view.background = resource
-                    }
-                })
         }
 
         fun bindSmallCardArrow(card: Card) {
-            if (card.formattedTitle != null) {
-                TextFormatter.applyFormattedText(
-                    card.formattedTitle, itemView.sca_tv_title, card.title)
-            } else {
-                itemView.bdc_tv_title.text = card.title
+            TextFormatter.applyFormattedText(
+                card.formattedTitle, itemView.sca_tv_title, card.title)
+
+            card.icon?.imageUrl?.let {
+                ImageHelper.loadImage(it, itemView, itemView.sca_iv_icon)
             }
-
-            Glide.with(itemView)
-                .load(card.icon?.imageUrl)
-                .into(object :
-                    CustomTarget<Drawable>() {
-                    override fun onLoadCleared(placeholder: Drawable?) {}
-
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        transition: Transition<in Drawable>?) {
-                        itemView.sca_iv_icon.background = resource
-                    }
-                })
         }
 
         fun bindImageCard(card: Card) {
-            Glide.with(itemView)
-                .load(card.bgImage?.imageUrl)
-                .into(object :
-                    CustomTarget<Drawable>() {
-                    override fun onLoadCleared(placeholder: Drawable?) {}
-
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        transition: Transition<in Drawable>?) {
-                        itemView.ic_card_view.background = resource
-                    }
-                })
+            card.bgImage?.imageUrl?.let {
+                ImageHelper.loadImage(it, itemView, itemView.ic_card_view)
+            }
         }
 
         fun bindCenterCard(card: Card) {
+            TextFormatter.applyFormattedText(
+                card.formattedTitle, itemView.cc_tv_title, card.title)
 
+            TextFormatter.applyFormattedText(
+                card.formattedDescription, itemView.cc_tv_description, card.description)
+
+            val gradient = GradientDrawable()
+            gradient.colors = card.bgGradient?.colorList?.map { Color.parseColor(it) }?.toIntArray()
+            gradient.shape = GradientDrawable.RECTANGLE
+            itemView.cc_linear_layout.setBackgroundDrawable(gradient)
+
+            card.icon?.imageUrl?.let {
+                ImageHelper.loadImage(it, itemView, itemView.cc_iv_icon)
+            }
         }
 
         fun bindSmallCard(card: Card) {
+            TextFormatter.applyFormattedText(
+                card.formattedTitle, itemView.sdc_tv_title, card.title)
 
+            TextFormatter.applyFormattedText(
+                card.formattedDescription, itemView.sdc_tv_description, card.description)
+
+            itemView.sdc_linear_layout.setBackgroundColor(Color.parseColor(card.bgColor))
+
+            card.icon?.imageUrl?.let {
+                ImageHelper.loadImage(it, itemView, itemView.sdc_iv_icon)
+            }
         }
     }
 }
